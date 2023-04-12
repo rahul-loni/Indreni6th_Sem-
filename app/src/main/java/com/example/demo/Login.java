945719fbba55fadc33e56b8e1d2c764f0ffd02ec
Rahul Kumar <rahulloni000@gmail.com>
2023-04-12 06:50:53 +0545

    Login Comp

--- app/src/main/java/com/example/demo/Login.java
+++ app/src/main/java/com/example/demo/Login.java
@@ -1,41 +1,62 @@
 package com.example.demo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class Login extends AppCompatActivity {
+    EditText txt_email,txt_password;
     Button login_btn;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth=FirebaseAuth.getInstance();
+
         init();
-    }
-    public void init(){
-        login_btn=findViewById(R.id.btn1);
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent_Login=new Intent(Login.this,MainActivity.class);
-                startActivity(intent_Login);
-                Toast.makeText(Login.this, "Login Comp", Toast.LENGTH_SHORT).show();
+                String email=txt_email.getText().toString().trim();
+                String password=txt_password.getText().toString();
+                if (email.isEmpty()){
+                    Toast.makeText(Login.this, "Please Enter Email", Toast.LENGTH_SHORT).show();
+                }
+                if (password.isEmpty()){
+                    Toast.makeText(Login.this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+                    auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Intent loginIntent=new Intent(getApplicationContext(),MainActivity.class);
+                                startActivity(loginIntent);
+                                Toast.makeText(Login.this, "Login Comp", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }else {
+                                Toast.makeText(Login.this, "Logon Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
             }
         });
     }
-
-    public void signup(View view) {
-        Intent intent_signup=new Intent(Login.this,Signup.class);
-        startActivity(intent_signup);
-    }
-
-    public void Forgot(View view) {
-        Intent forgot_intent=new Intent(Login.this,Forgot.class);
-        startActivity(forgot_intent);
+    public void init(){
+        txt_email=findViewById(R.id.email);
+        txt_password=findViewById(R.id.password);
+        login_btn=findViewById(R.id.btn_login);
     }
 }
